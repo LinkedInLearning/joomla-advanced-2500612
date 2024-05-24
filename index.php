d0951476500a4b2315be0bfdf14c7944e71f847b
timschuermann <tischuer@web.de>
2024-05-24 19:34:59 +0200

    Add files

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title>Odenwälder Rezepte</title>
+  </head>
+  
+  <body>
+  
+    <h1>Odenwälder Rezepte</h1>
+
+    <p>Hier folgt der Beitrag.</p>
+
+  </body>
+
+</html>
+
