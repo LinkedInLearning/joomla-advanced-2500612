0418ab4d92f7906fd6dd5393fe31792df1208d11
timschuermann <tischuer@web.de>
2024-05-24 19:47:22 +0200

    Add files for video 03_11

---
+++ 03_11/index.php
@@ -0,0 +1,22 @@
+<?php $this->getWebAssetManager()->registerAndUseStyle('rezepte', '/templates/' . $this->template . '/css/template.css'); ?>
+<!DOCTYPE html>
+<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+
+  <head>
+    <jdoc:include type="head" />
+  </head>
+  
+  <body>
+  
+	<h1><?php echo JFactory::getApplication()->get('sitename'); ?></h1>
+	
+	<div> <jdoc:include type="modules" name="menu" /> </div> <!-- Hauptmenü -->
+
+	<div> <jdoc:include type="component" /> </div> <!-- Beitrag -->
+
+	<div> <jdoc:include type="modules" name="sidebar-right" /> </div> <!-- Module -->
+	
+  </body>
+
+</html>
+
