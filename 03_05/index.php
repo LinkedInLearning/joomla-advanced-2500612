fac566b8352e8e71c8133ca9e81bdfa666102f99
timschuermann <tischuer@web.de>
2024-05-24 19:41:19 +0200

    Add file for 03_05

---
+++ 03_05/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+
+  <head>
+    <jdoc:include type="head" />
+  </head>
+  
+  <body>
+  
+	<h1>Odenwälder Rezepte</h1>
+	
+	<div> </div> <!-- Hauptmenü -->
+
+	<div> </div> <!-- Beitrag -->
+
+	<div> </div> <!-- Module -->
+	
+  </body>
+
+</html>
+
