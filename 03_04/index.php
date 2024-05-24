844df9ca32e367150ca1f46349f2ee072b1803f3
timschuermann <tischuer@web.de>
2024-05-24 19:37:48 +0200

    Add file for video 03_04

---
+++ 03_04/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+
+  <head>
+    <jdoc:include type="head" />
+  </head>
+  
+  <body>
+  
+    <h1>Odenwälder Rezepte</h1>
+  
+    <p>Hier folgt der Beitrag.</p>
+  
+  </body>
+
+</html>
+
